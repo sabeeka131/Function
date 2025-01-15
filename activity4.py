50c3b1363411a938e1aa8b3f8fa84e659323e9c6
Aliza <59551300+sabeeka131@users.noreply.github.com>
2025-01-15 18:46:26 +0500

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,5 @@
+def circumference(radius):
+   return 2 * 3.14159 * radius
+
+radius = float(input("Please enter the radius of the circle: "))
+print("The circumference of the circle is:", circumference(radius))
